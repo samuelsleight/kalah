38866a0f43316edcc1817bc8bef2fa8f9137c5a7
AndrewHynes <andrew.hynes@hotmail.co.uk>
2013-12-04 17:18:39 +0000

    Added Tree class

---
+++ Code/Tree.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+ 
+public class Tree<T> {
+        private Tree<T> parent;
+        private ArrayList<Tree<T>> children;
+        private T data;
+ 
+        public Tree(Tree<T> parent, T data) {
+                this.parent = parent;
+                this.data = data;
+                this.children = new ArrayList<Tree<T>>();
+ 
+        }
+ 
+        public void addChild(Tree<T> child) {
+                children.add(child);
+ 
+        }
+ 
+        public ArrayList<Tree<T>> getChildren() {
+                return children;
+ 
+        }
+ 
+        public void setData(T value) {
+                this.data = value;
+ 
+        }
+ 
+        public T getData() {
+                return data;
+ 
+        }
+ 
+        public void setParent(Tree<T> parent) {
+                this.parent = parent;
+ 
+        }
+ 
+        public Tree<T> getParent() {
+                return parent;
+ 
+        }
+ 
+}
