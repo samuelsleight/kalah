a9b455f98bf967ba95f1686c2d750a74f4aa91fa
AndrewHynes <andrew.hynes@hotmail.co.uk>
2013-12-07 21:40:01 +0000

    Added more frameworky stuff.

--- Code/MASH.java
+++ Code/MASH.java
@@ -1,7 +1,7 @@
 
 public class MASH extends AIBase //MASH Algorithm - Mega Autonomous Sexy Heuristic Algorithm
 {
-	private int currentState;
+	private KalahGame currentState;
 
 	MASH(KalahGame g, int playerID)
 	{
@@ -15,7 +15,29 @@ public class MASH extends AIBase //MASH Algorithm - Mega Autonomous Sexy Heurist
 		return 0;
 	}
 
-	private void createTree()
+	private Tree createTree()
+	{
+		Tree tree = new Tree(null, 1);
+		for(int i = 0; i < 6; i++)
+		{
+			//sow
+
+			if(true) //option is good continue searching - this is where we check by heuristic
+			{
+				//recursive call (?)
+			}
+			else
+			{
+				//set game back to current state
+			}
+		}
+
+		//do pruning (via stricter heuristic?) here
+
+		return tree; //yes, yes, a *real* tree needs to be made
+	}
+
+	private void parseTree() //for useful information
 	{
 		
 	}
