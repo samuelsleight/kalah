cbb365ea41fe630ee6fc5df01925802816284809
AndrewHynes <andrew.hynes@hotmail.co.uk>
2013-12-09 17:50:36 +0000

    AN ACTUAL AI
    (Well...)

--- Code/MASH.java
+++ Code/MASH.java
@@ -1,18 +1,48 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Random;
 
 public class MASH extends AIBase //MASH Algorithm - Mega Autonomous Sexy Heuristic Algorithm
 {
-	private KalahGame currentState;
+	private Map<GameState, ProbArray> memory;
+	private Random r;
 
 	MASH(KalahGame g, int playerID)
 	{
 		super(g, playerID);
+
+		memory = new HashMap<GameState, ProbArray>();
+		r = new Random();
 		
 	}
 
 	public int makeMove()
 	{
-		//dostuff
-		return 0;
+		int[] moves = game.getAllowedMoves(getPlayerID());
+		GameState s = new GameState(moves, getPlayerID());
+		if(!(memory.containsKey(s))) {
+			double[] probs = new double[moves.length];
+			for(int i = 0; i < moves.length; i++) {
+				probs[i] = 1.0 / (double)(moves.length);
+
+			}
+
+			memory.put(s, new ProbArray(probs));
+
+		}
+
+		ProbArray probs = memory.get(s);
+		double val = r.nextDouble();
+		double accum = 0;
+		int i = 0;
+		for(; i < probs.getSize(); i++) {
+			accum += probs.get(i);
+			if(val < accum) {
+				break;
+			}
+		}
+
+		return moves[i];
 	}
 
 	private Tree createTree()
@@ -48,4 +78,36 @@ public class MASH extends AIBase //MASH Algorithm - Mega Autonomous Sexy Heurist
 	}
 
 	
+
+private class GameState {
+	private int[] availableMoves;
+	private int player;
+
+	public GameState(int[] availbleMoves, int player) {
+		this.availableMoves = availableMoves;
+		player = player;
+
+	}
+
+}
+
+private class ProbArray {
+	private double[] probs;
+
+	public ProbArray(double[] probs) {
+		this.probs = probs;
+
+	}
+
+	public int getSize() {
+		return probs.length;
+
+	}
+
+	public double get(int i) {
+		return probs[i];
+
+	}
+}
+
 }
--- Code/Test.java
+++ Code/Test.java
@@ -4,9 +4,16 @@ public class Test {
 	public static void main(String[] args) {
 		KalahGame game = new KalahGame();
 
+		/*
 		AIBase firstPlayer = new Player(game, KalahGame.PLAYER_1);
 		AIBase secondPlayer = new Player(game, KalahGame.PLAYER_2);
+		*/
+
+		AIBase firstPlayer = new MASH(game, KalahGame.PLAYER_1);
+		AIBase secondPlayer = new MASH(game, KalahGame.PLAYER_2);
 		
+		//for(int i = 0; i < 1000; i++) {
+
 		int result = game.playGame(firstPlayer, secondPlayer);
 		
 		System.out.println("FINAL STATE");
@@ -16,6 +23,9 @@ public class Test {
 			case 0: System.out.println("Draw");		break;
 			case 1: System.out.println("Player 1 Wins");	break;
 			case 2: System.out.println("Player 2 Wins");	break;
+
+		//}
+
 		}
 	}
 }
